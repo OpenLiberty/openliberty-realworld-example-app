649354a7f44f33a37e5d237c9c0c80c30a6f116b
asjervanasten <asjer94@live.com>
2023-09-15 21:05:44 +0200

    Being able to run the integrationtests by configuring RestAssured and adding test microprofile config

--- src/test/java/org/example/realworldapi/AbstractIntegrationTest.java
+++ src/test/java/org/example/realworldapi/AbstractIntegrationTest.java
@@ -2,7 +2,10 @@ package org.example.realworldapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.slugify.Slugify;
+import de.hamburger_software.util.junit.microprofile.config.MicroProfileConfigExtension;
+import io.restassured.RestAssured;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.example.realworldapi.infrastructure.provider.JwtTokenProvider;
 import org.example.realworldapi.infrastructure.repository.hibernate.entity.*;
 import org.example.realworldapi.infrastructure.web.provider.TokenProvider;
@@ -10,20 +13,31 @@ import org.example.realworldapi.util.UserEntityUtils;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
 @EnableAutoWeld
-@AddBeanClasses(JwtTokenProvider.class)
+@AddBeanClasses(ObjectMapper.class)
+@ExtendWith(MicroProfileConfigExtension.class)
 public class AbstractIntegrationTest extends DatabaseIntegrationTest {
 
     @Inject
     protected ObjectMapper objectMapper;
-    @Inject
-    protected TokenProvider tokenProvider;
+    protected static TokenProvider tokenProvider;
     protected Slugify slugify = Slugify.builder().build();
+
+    @BeforeAll
+    public static void beforeAll(@ConfigProperty(name = "jwt.issuer") String issuer,
+                                 @ConfigProperty(name = "jwt.secret") String secret,
+                                 @ConfigProperty(name = "jwt.expiration.time.minutes") Integer expirationTimeInMinutes) {
+        tokenProvider = new JwtTokenProvider(issuer, secret, expirationTimeInMinutes);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 9080;
+    }
 
     @AfterEach
     public void afterEach() {
